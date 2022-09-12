fdbd083636386232fb9d9a6a216647ca5cc46487
John4E656F <jm49andres@gmail.com>
2022-09-12 16:19:52 +0200

    setup project

---
+++ client/src/routing.js
@@ -0,0 +1,25 @@
+//This is where we build the different states
+
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+
+//Import All the different States
+// import Name from "./path/to/file";
+
+const Routing = () => {
+
+    return (
+
+        <Routes>
+{/* 
+            <Route exact path="/" element={<Welcome />} />
+
+            <Route path="/watch">
+                <Route exact path=":id" element={<VideoPage />} />
+            </Route> */}
+
+        </Routes>
+
+    )
+}
+export default Routing;
